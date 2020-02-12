a99c42f0167d526d19b0d0b282f029273ce86749
Ken Sipe <kensipe@gmail.com>
2020-02-12 17:22:58 -0600

    adding desc to pod command
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- src/main/java/com/d2iq/kubectl/PodCommand.java
+++ src/main/java/com/d2iq/kubectl/PodCommand.java
@@ -4,6 +4,7 @@ import picocli.CommandLine;
 
 @CommandLine.Command(
         name = "pod",
+        description = "Programmatically accesses pods in a k8s with 'add', 'list' and 'list2'",
         subcommands = {
                 PodAddCommand.class,
                 PodListCommand.class,
